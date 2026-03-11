d11c0da2950d7f027a67fff62d11dc1a69a08a98
Fidel Thomet <git@ft0.ch>
2026-03-11 14:18:42 +0100

    editor config, removed boilerplate, base app structure

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Interface from '@/lib/Interface.svelte'
+  import Renderer from '@/lib/Renderer.svelte'
+</script>
+
+<main>
+  <Renderer />
+  <Interface />
+</main>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
