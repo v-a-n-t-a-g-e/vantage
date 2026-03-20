8a1bbd76a331ef1189e70bff81c06145f27f51c7
Fidel Thomet <git@ft0.ch>
2026-03-20 12:40:00 +0100

    improve ConfigBar UI

--- src/lib/ui/ConfigBar.svelte
+++ src/lib/ui/ConfigBar.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
   import { sceneState } from '@/lib/sceneState.svelte.ts'
+  import IconGrid from '@/assets/icons/Grid.svg'
 </script>
 
 <div class="ui-container pointer-events-auto flex h-10 shrink-0 items-center gap-3 px-3">
-  <label class="flex items-center gap-1.5 text-xs">
-    <input type="checkbox" bind:checked={sceneState.showGrid} />
-    Grid
-  </label>
+  <button
+    class="flex items-center transition-opacity"
+    class:text-brand={sceneState.showGrid}
+    onclick={() => (sceneState.showGrid = !sceneState.showGrid)}
+  >
+    <IconGrid />
+  </button>
 
-  <label class="flex items-center gap-1.5 text-xs">
-    BG
+  <label class="relative flex size-4 cursor-pointer items-center justify-center">
+    <span
+      style="background-color: {sceneState.clearColor}"
+      class="size-4 rounded-full border border-black"
+    ></span>
     <input
+      class="absolute inset-0 cursor-pointer opacity-0"
       type="color"
-      class="h-5 w-5 cursor-pointer border border-black p-0"
       bind:value={sceneState.clearColor}
     />
   </label>
