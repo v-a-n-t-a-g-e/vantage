2628b1bfc2bf97a0c3c1f74415e38ed315c126fa
Giacomo Nanni <gn.nanni@gmail.com>
2026-04-01 10:51:24 +0200

    fix increase ambient light intensity

--- src/lib/scene/DefaultEnvironment.ts
+++ src/lib/scene/DefaultEnvironment.ts
@@ -8,7 +8,7 @@ export class DefaultEnvironment extends THREE.Group {
     super()
     this.grid = new InfiniteGrid()
     this.add(this.grid)
-    this.add(new THREE.AmbientLight(0xffffff, 0.6))
+    this.add(new THREE.AmbientLight(0xffffff, 2.0))
     const dir = new THREE.DirectionalLight(0xffffff, 1.2)
     dir.position.set(10, 20, 10)
     this.add(dir)
